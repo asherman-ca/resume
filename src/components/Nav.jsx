e94100a341359a6487b6bb47cff023febec9bfe0
Alex Sherman <asherman.ca@gmail.com>
2022-05-30 14:56:46 -0700

    header font size

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
 		<div className='nav-container'>
 			<div className='header'>
 				<img src={headShot} alt='' />
-				<span>Software Engineer</span>
+				<span>Alex Sherman</span>
 			</div>
 			<div className='links'>
 				<div className='tooltip'>
@@ -39,7 +39,7 @@ const Nav = () => {
 					<i className='fa-solid fa-square-phone'></i>
 					<span className='tooltip-text'>415.652.0317</span>
 				</div>
-				<span className='tooltip-default'>Alex Sherman</span>
+				{/* <span className='tooltip-default'>Alex Sherman</span> */}
 			</div>
 		</div>
 	);
