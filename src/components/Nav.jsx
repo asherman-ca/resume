f966718ed3ff971ad276c31bc3689c9ba1d1b929
Alex Sherman <asherman.ca@gmail.com>
2022-05-29 17:30:19 -0700

    nav links refactor

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,22 +3,27 @@ import React from 'react';
 const Nav = () => {
 	return (
 		<div className='nav-container'>
-			<span>
-				<a
-					href='https://github.com/asherman-ca'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<i className='fa-brands fa-github' /> Alex Sherman
-				</a>
-			</span>
+			<span>Alex Sherman</span>
 			<div>
-				<i className='fa-solid fa-envelope tooltip'>
+				<div className='tooltip'>
+					<a
+						href='https://github.com/asherman-ca'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<i className='fa-brands fa-github' />
+					</a>
+					<span className='tooltip-text'>https://github.com/asherman-ca</span>
+				</div>
+				<div className='tooltip'>
+					<i className='fa-solid fa-envelope'></i>
 					<span className='tooltip-text'>asherman.ca@gmail.com</span>
-				</i>
-				<i className='fa-solid fa-phone tooltip'>
+				</div>
+				<div className='tooltip'>
+					<i className='fa-solid fa-phone tooltip'></i>
 					<span className='tooltip-text'>415.652.0317</span>
-				</i>
+				</div>
+
 				{/* <span>asherman.ca@gmail.com</span> */}
 				{/* <span>415.652.0317</span> */}
 			</div>
