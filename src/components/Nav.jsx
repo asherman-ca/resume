69ca89c50e2266a1a5d233020f22c403cf269ea9
Alex Sherman <asherman.ca@gmail.com>
2022-05-29 17:14:29 -0700

    email and phone hover effects

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,25 @@ import React from 'react';
 const Nav = () => {
 	return (
 		<div className='nav-container'>
-			<span>Alex Sherman</span>
-			<span>asherman.ca@gmail.com</span>
-			<span>4156520317</span>
+			<span>
+				<a
+					href='https://github.com/asherman-ca'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<i className='fa-brands fa-github' /> Alex Sherman
+				</a>
+			</span>
+			<div>
+				<i className='fa-solid fa-envelope tooltip'>
+					<span className='tooltip-text'>asherman.ca@gmail.com</span>
+				</i>
+				<i className='fa-solid fa-phone tooltip'>
+					<span className='tooltip-text'>415.652.0317</span>
+				</i>
+				{/* <span>asherman.ca@gmail.com</span> */}
+				{/* <span>415.652.0317</span> */}
+			</div>
 		</div>
 	);
 };
--- src/components/ResumeListItem.jsx
+++ src/components/ResumeListItem.jsx
@@ -5,11 +5,11 @@ const ResumeListItem = () => {
 	return (
 		<div className='resume-list-item'>
 			<div className='header'>
-				ResumeListItem
 				<i
 					onClick={() => setShow(!show)}
 					className={show ? 'fa-solid fa-folder-open' : 'fa-solid fa-folder'}
-				></i>
+				/>
+				<span className='title'>Title</span>
 			</div>
 			{show && <div className='details'>details</div>}
 		</div>
